9a45b62bd73591233f58fb7eb2433f0c94e4da93
amtoaer <amtoaer@gmail.com>
2021-08-20 17:52:08 +0800

    feat: 加入注释，补全主函数运行流程

--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"github.com/amtoaer/bing-bong/client"
+	"github.com/amtoaer/bing-bong/message"
+	"github.com/amtoaer/bing-bong/model"
 	"github.com/spf13/viper"
 )
 
 type robot interface {
 	Login(string, string)
-	HandleEvent()
+	SendMessage(int64, string, bool)
+	HandleEvent(*message.MessageQueue)
 }
 
 func main() {
-	var bot robot
+	var (
+		bot robot               //机器人实例
+		mq  = message.Default() //消息队列
+	)
+	// 根据机器人类型决定实例化内容
 	switch viper.GetString("botType") {
 	case "qq":
 		bot = &client.QQ{}
 	}
-	bot.Login(viper.GetString("account"), viper.GetString("password"))
-	bot.HandleEvent()
+	// 登录机器人
+	bot.Login(viper.GetString("botUser"), viper.GetString("botPass"))
+	// 从数据库读取订阅信息初始化消息队列
+	model.InitMessageQueue(bot, mq)
+	// 启动机器人外部事件监听
+	bot.HandleEvent(mq)
 }
