43bf553a7534a87b1228bbc10ed47c4475c91e32
amtoaer <amtoaer@gmail.com>
2021-08-22 20:50:50 +0800

    chore: 使用第三方log包检测错误并打印

--- utils/error.go
+++ utils/error.go
@@ -1,12 +1,23 @@
 package utils
 
-import "log"
+import log "github.com/sirupsen/logrus"
 
 // 检测一系列错误，如发现错误则退出程序
-func CheckError(msg string, errs ...error) {
+func Fatalf(msg string, errs ...error) {
 	for _, err := range errs {
 		if err != nil {
 			log.Fatalf(msg, err)
 		}
 	}
+}
+
+func Errorf(msg string, errs ...error) bool {
+	var flag bool
+	for _, err := range errs {
+		if err != nil {
+			log.Errorf(msg, err)
+			flag = true
+		}
+	}
+	return flag
 }
