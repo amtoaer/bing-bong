f9e429bbda8ef98505676bdeac44ae4cf23aee78
amtoaer <amtoaer@gmail.com>
2021-08-22 20:48:42 +0800

    feat: 实现QQ机器人的命令交互

---
+++ client/qq.go
@@ -0,0 +1,149 @@
+package client
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/amtoaer/bing-bong/internal"
+	"github.com/amtoaer/bing-bong/message"
+	"github.com/amtoaer/bing-bong/model"
+	"github.com/amtoaer/bing-bong/utils"
+	"github.com/asaskevich/govalidator"
+	log "github.com/sirupsen/logrus"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/driver"
+	"github.com/wdvxdr1123/ZeroBot/extension"
+)
+
+type QQ struct {
+	bot  *zero.Ctx
+	Conf map[string]interface{}
+}
+
+func (q *QQ) Init() {
+	var (
+		account     = q.Conf["account"].(string)
+		webSocket   = q.Conf["websocket"].(string)
+		accessToken = q.Conf["accesstoken"].(string)
+	)
+	zero.Run(zero.Config{
+		CommandPrefix: "/",
+		Driver: []zero.Driver{
+			driver.NewWebSocketClient(webSocket, accessToken),
+		},
+		SelfID: account,
+	})
+	accountNum, err := strconv.ParseInt(account, 10, 64)
+	utils.Fatalf("error parsing account:%v", err)
+	time.Sleep(1 * time.Second) // zero内部的机器人注册是异步的，这里暂且使用sleep等待1s，防止zero.GetBot为nil
+	q.bot = zero.GetBot(accountNum)
+	if q.bot != nil {
+		log.Info("机器人成功启动")
+	} else {
+		log.Fatalf("机器人启动失败") //如果启动失败可以适当延长上面的sleep时间试试（（
+	}
+}
+
+// 发送消息
+func (q *QQ) SendMessage(userID int64, message string, isGroup bool) {
+	if isGroup {
+		q.bot.SendGroupMessage(userID, message)
+	} else {
+		q.bot.SendPrivateMessage(userID, message)
+	}
+}
+
+// 监听事件并阻塞程序
+func (q *QQ) HandleEvent(mq *message.MessageQueue) {
+	zero.OnCommandGroup([]string{"订阅", "subscribe"}).Handle(func(ctx *zero.Ctx) {
+		var cmd extension.CommandModel
+		err := ctx.Parse(&cmd)
+		if utils.Errorf("处理命令时发生错误：%v", err) {
+			return
+		}
+		if cmd.Args == "" {
+			ctx.Send("请输入要订阅的链接！")
+		} else {
+			if !govalidator.IsURL(cmd.Args) {
+				ctx.Send("请输入合法的链接！")
+			} else {
+				ctx.Send("获取feeds信息中...")
+				title, err := internal.ParseTitle(cmd.Args)
+				if utils.Errorf("error getting feeds title:%v", err) {
+					return
+				}
+				isGroup, userID := getCtxInfo(ctx)
+				mq.Subscribe(q, cmd.Args, userID, isGroup)
+				model.InsertSubscription(cmd.Args, title, userID, isGroup)
+				ctx.Send(fmt.Sprintf("订阅《%s》成功！", title))
+			}
+		}
+	})
+	zero.OnCommandGroup([]string{"取消订阅", "unsubscribe"}).Handle(func(ctx *zero.Ctx) {
+		isGroup, userID := getCtxInfo(ctx)
+		feeds := model.QueryFeed(userID)
+		hasUrl, messageToSend := buildMessage(feeds, false)
+		if !hasUrl {
+			ctx.Send("您还没有任何订阅内容！")
+		} else {
+			ctx.Send(messageToSend)
+			next := ctx.FutureEvent("message", ctx.CheckSession())
+			recv, cancel := next.Repeat()
+			time.AfterFunc(10*time.Second, cancel)
+			for event := range recv {
+				numStr := event.Message.ExtractPlainText()
+				if num, err := strconv.Atoi(numStr); err != nil {
+					ctx.Send("请确保输入数字！")
+				} else {
+					num -= 1
+					if num >= 0 && num < len(feeds) {
+						model.DeleteSubscription(feeds[num].Url, userID, isGroup)
+						mq.Unsubscribe(feeds[num].Url, userID)
+						ctx.Send("删除订阅成功！")
+						break
+					} else {
+						ctx.Send("编号错误，请重试！")
+					}
+				}
+			}
+		}
+	})
+	zero.OnCommandGroup([]string{"查询订阅", "searchSubscription"}).Handle(func(ctx *zero.Ctx) {
+		_, userID := getCtxInfo(ctx)
+		urls := model.QueryFeed(userID)
+		hasUrl, messageToSend := buildMessage(urls, true)
+		if !hasUrl {
+			ctx.Send("您还没有任何订阅内容！")
+		} else {
+			ctx.Send(messageToSend)
+		}
+	})
+	select {}
+}
+
+// 判断是否是group，获得userID
+func getCtxInfo(ctx *zero.Ctx) (isGroup bool, userID int64) {
+	if ctx.Event.GroupID != 0 {
+		isGroup = true
+		userID = ctx.Event.GroupID
+	} else {
+		isGroup = false
+		userID = ctx.Event.UserID
+	}
+	return
+}
+
+func buildMessage(feeds []*model.Feed, isQuery bool) (hasFeed bool, message string) {
+	if len(feeds) == 0 {
+		return
+	}
+	hasFeed = true
+	for index, feed := range feeds {
+		message += fmt.Sprintf("%d. %s \n", index+1, feed.Name)
+	}
+	if !isQuery {
+		message += "请在十秒内输入您要取消的编号。"
+	}
+	return
+}
--- main.go
+++ main.go
@@ -2,31 +2,36 @@ package main
 
 import (
 	"github.com/amtoaer/bing-bong/client"
+	"github.com/amtoaer/bing-bong/internal"
 	"github.com/amtoaer/bing-bong/message"
 	"github.com/amtoaer/bing-bong/model"
 	"github.com/spf13/viper"
 )
 
 type robot interface {
-	Login(string, string)
+	Init()
 	SendMessage(int64, string, bool)
 	HandleEvent(*message.MessageQueue)
 }
 
 func main() {
 	var (
-		bot robot               //机器人实例
-		mq  = message.Default() //消息队列
+		bot     robot                  //机器人实例
+		mq      = message.Default()    //消息队列
+		botConf map[string]interface{} //机器人配置
 	)
 	// 根据机器人类型决定实例化内容
 	switch viper.GetString("botType") {
 	case "qq":
-		bot = &client.QQ{}
+		botConf = viper.GetStringMap("qq")
+		bot = &client.QQ{Conf: botConf}
 	}
 	// 登录机器人
-	bot.Login(viper.GetString("botUser"), viper.GetString("botPass"))
+	bot.Init()
 	// 从数据库读取订阅信息初始化消息队列
 	model.InitMessageQueue(bot, mq)
+	// 开始检测rss更新
+	go internal.CheckMessage(mq)
 	// 启动机器人外部事件监听
 	bot.HandleEvent(mq)
 }
